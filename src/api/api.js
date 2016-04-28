8270864e228a02b863705432bf561fdc34c7b1e2
alexisjanvier <contact@alexisjanvier.net>
2016-04-28 07:02:27 +0200

    Add amazon search endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,30 @@
 import config from 'config';
 import koa from 'koa';
 import koaRoute from 'koa-route';
+import amazon from 'amazon-product-api';
 
 const app = koa();
+
+const client = amazon.createClient({
+    awsTag: config.amazon.AWS_TAG,
+    awsId: config.amazon.AWS_ID,
+    awsSecret: config.amazon.AWS_SECRET,
+});
 
 app.use(koaRoute.get('/', function* primaryEntryPoint() {
     this.status = 200;
     this.body = { status: 'ok' };
+}));
+
+app.use(koaRoute.get('/amazon/:isbn', function* searchAmazon(isbn) {
+    console.log('JE CHERCHE');
+    console.log(isbn);
+    this.body = yield client.itemSearch({
+        keywords: isbn,
+        searchIndex: 'Music',
+        idType: 'ISBN',
+        responseGroup: 'ItemAttributes,Offers,Images',
+    });
 }));
 
 export default app;
